345ffd58139ec5fb7788967abaf8688196406b82
MatthewMarkov <mathew.markov@gmail.com>
2020-05-06 13:42:45 +0300

    HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store';
@@ -10,9 +10,9 @@ import store from './redux/redux-store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>, document.getElementById('root'),
 );
 
