b3d91336ef6bad36d5f418953d74c3a9f43f312c
MatthewMarkov <mathew.markov@gmail.com>
2020-05-06 12:43:05 +0300

    Updates

---
+++ precache-manifest.010d46b272a8a1ef76102375f83c53d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835f517c563cceeba335267982a3f853",
+    "url": "/my-social-media/index.html"
+  },
+  {
+    "revision": "74aa9b8810c399b51d55",
+    "url": "/my-social-media/static/css/3.d0a2d463.chunk.css"
+  },
+  {
+    "revision": "fdf2d8f2e6d4f3476188",
+    "url": "/my-social-media/static/css/main.aff4d3cb.chunk.css"
+  },
+  {
+    "revision": "cb4d8ca5aada220c9735",
+    "url": "/my-social-media/static/js/2.56f318b8.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/my-social-media/static/js/2.56f318b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74aa9b8810c399b51d55",
+    "url": "/my-social-media/static/js/3.0af33a09.chunk.js"
+  },
+  {
+    "revision": "fdf2d8f2e6d4f3476188",
+    "url": "/my-social-media/static/js/main.c6d225e2.chunk.js"
+  },
+  {
+    "revision": "8c7102bb9e12051621f1",
+    "url": "/my-social-media/static/js/runtime-main.a60c5615.js"
+  }
+]);
