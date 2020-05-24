e4c1916dd2c678643a860ab6e8cb0f31796d28c5
MatthewMarkov <mathew.markov@gmail.com>
2020-05-24 15:49:46 +0300

    Updates

---
+++ precache-manifest.6c80dbab2cc8204675c966aa6566d671.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad489579f6c2d9f6d8685a14c4e7010",
+    "url": "/my-social-media/index.html"
+  },
+  {
+    "revision": "0378ce8fb8dfa93fe583",
+    "url": "/my-social-media/static/css/3.64d8a83d.chunk.css"
+  },
+  {
+    "revision": "c9a8786403490e19fa14",
+    "url": "/my-social-media/static/css/main.c1aece8c.chunk.css"
+  },
+  {
+    "revision": "5828cd472a782ad7fe19",
+    "url": "/my-social-media/static/js/2.854ec10c.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/my-social-media/static/js/2.854ec10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0378ce8fb8dfa93fe583",
+    "url": "/my-social-media/static/js/3.d2fe1ca2.chunk.js"
+  },
+  {
+    "revision": "c9a8786403490e19fa14",
+    "url": "/my-social-media/static/js/main.6a1d1917.chunk.js"
+  },
+  {
+    "revision": "f0812e4e8adbc7d2fefd",
+    "url": "/my-social-media/static/js/runtime-main.42dd7f77.js"
+  },
+  {
+    "revision": "258a9e69a7fed9f26c79a912d3c113c1",
+    "url": "/my-social-media/static/media/vk-1.258a9e69.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-social-media/precache-manifest.c55d83622eba60258c607e56c8feba13.js"
+  "/my-social-media/precache-manifest.6c80dbab2cc8204675c966aa6566d671.js"
 );
 
 self.addEventListener('message', (event) => {
